b39c66e99467e270a51c1e9f4e820b807ef1f990
hdm007 <harry.mardirossian13@gmail.com>
2021-06-01 18:21:43 -0400

    prepared app for implementing the backpack modal

--- src/features/game/Game.js
+++ src/features/game/Game.js
@@ -11,10 +11,15 @@ export default function Game() {
             <section aria-label="travel buttons">
                 <h2> Travel Buttons </h2>
                 <p> These change the x-and-y values in state by 1 or -1 based on the cardinal direction.</p>
+                <p>You are at point [ {game.xCoordinate}, {game.yCoordinate} ] on the grid. </p>
                 <button aria-label="travel north" onClick = {() => {dispatch(travelNorth())}}> Travel North </button>
                 <button aria-label="travel south" onClick = {() => {dispatch(travelSouth())}}> Travel South </button>
                 <button aria-label="travel east" onClick = {() => {dispatch(travelEast())}}> Travel East </button>
                 <button aria-label="travel west" onClick = {() => {dispatch(travelWest())}}> Travel West </button>
+            </section>
+
+            <section aria-label="backpack toggle">
+                <button> Backpack </button>
             </section>
         </div>
     )
